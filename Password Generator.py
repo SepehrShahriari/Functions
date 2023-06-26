f182a5fc79de5abefa08368581d5c63ce91322bd
M.Sepehr Shahriyari <108497345+SepehrShahriari@users.noreply.github.com>
2023-06-26 20:43:10 +0330

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -9,13 +9,10 @@ import string # for grab all lower and uppercase letter + special characters +  
 #special_characters=True : means the password includs characters( user chooses )
 def generate_password( min_lenght , numbers=True, special_characters=True):
     print(numbers, special_characters)
-    #here we call letters,numbers and punctuations, then put them in a variable (like: abcdefgABCDEFG)
+    #here we call letters,numbers and punctuations, then put them in differente variable (like: abcdefgABCDEFG)
     letter = string.ascii_letters # all lower and uppercase letters (like: abcdefgABCDEFG)
-    print(letter)
     digits = string.digits # all numbers (1234567890)
-    print(digits)
     special = string.punctuation # all punctuations (like: !"#$%&'()*+,-./ )
-    print(special)
 
     password_ = letter # ouser password atleast will include letters
     print(password_)
@@ -41,12 +38,11 @@ def generate_password( min_lenght , numbers=True, special_characters=True):
             has_special = True
             
         meets_criteria = True
-        
         if numbers: # number == True
             meets_criteria = has_number
         if special_characters: # special_character == True
             meets_criteria = meets_criteria and has_special
-
+            
     return pwd
 
 min_lenght = int(input("enter the minimum lenght: "))
@@ -54,16 +50,6 @@ has_number = input("Do you want to have numbers (y/n) ? ").lower() == "y"
 print(has_number)
 has_special = input("Do you want to have special character (y/n) ? ").lower() == "y"
 print(has_special)
-
-"""if has_number == "y":
-
-elif has_special == "y":
-
-elif has_special == "y" and has_number == "y"
-
-else :
-    print("enter correct value")
-    continue"""
 
 pwd = generate_password(min_lenght, has_number, has_special)
 print("your password is: ", pwd)
